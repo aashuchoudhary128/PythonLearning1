e1fa7b3e1b742c18c487f4e8a071875763ed564c
Aashu Choudhary <aashuchoudhary128@gmail.com>
2026-07-14 22:30:58 +0530

    type conversion

---
+++ Type conversion.py
@@ -0,0 +1,16 @@
+#convert from int to float:
+x = float(10)
+
+#convert from float to int:
+y = int(1.8)
+
+#convert from int to complex:
+z = complex(1)
+
+print(x)
+print(y)
+print(z)
+
+print(type(x))
+print(type(y))
+print(type(z))
