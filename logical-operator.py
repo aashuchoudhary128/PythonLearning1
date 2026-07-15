511b4e1bbc66703730ccccf35378944a987e2ee1
Aashu Choudhary <aashuchoudhary128@gmail.com>
2026-07-15 22:46:56 +0530

    logical operators

---
+++ logical-operator.py
@@ -0,0 +1,5 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+print("a > 0 and b > 0 :", a > 0 and b > 0)
+print("a > 0 or b > 0  :", a > 0 or b > 0)
+print("not(a > b)      :", not(a > b))
