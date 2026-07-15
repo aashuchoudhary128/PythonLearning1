30ccb05ab59b748af82caeea7930762b6b145224
Aashu Choudhary <aashuchoudhary128@gmail.com>
2026-07-15 22:42:53 +0530

    relation operators

---
+++ Relational-operator.py
@@ -0,0 +1,8 @@
+a=int(int(input("Enter the first number:")))
+b=int(int(input("Enter the second number:")))
+print("a == b: ",a == b)
+print("a > b: ",a > b)
+print("a < b: ",a < b)
+print("a != b: ",a != b)
+print("a >= b: ",a >= b)
+print("a <= b: ",a <= b)
