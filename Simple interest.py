b7cdff065084b25facfcf83685e00ac5558c89b3
Aashu Choudhary <aashuchoudhary128@gmail.com>
2026-07-17 18:02:59 +0530

    SI

---
+++ Simple interest.py
@@ -0,0 +1,5 @@
+principle=float(input("Enter principle amount: "))
+rate=float(input("Enter rate (%): "))
+time=float(input("Enter the time (years): "))
+SI =(principle+rate+time)*100
+print("The SI is: ",SI)
